f4fe4dff7db2056d4421b4d5d4a44fcf1efbcad1
Michael Klishevich <busation@gmail.com>
2019-03-31 20:47:53 +0300

    update

--- aa2/this.js
+++ aa2/this.js
@@ -1,21 +1,26 @@
-// ({
-//   method: function() {
-//     (
-//       function() {
-//         console.log(this);
-//       }
-//     )();
-//   }
-// }).method();
-
 const a = {
-  method: function() {
-    (
-      function() {
-        console.log(this);
-      }
-    )();
+  // example 1
+  name: 'Awesome Name',
+  method1: function() {
+    console.log(this.name);
+  },
+  // example 2
+  method2: () => {
+    console.log(this.name);
+  },
+  // example 3
+  method3: function() {
+    (function() {
+      console.log(this.name);
+    })()
   }
 }
 
-console.log(a);
+a.method1();
+
+// example 4
+// function f2 () {
+//   'use strict'; 
+//   return console.log(this);
+// }
+// f2();
